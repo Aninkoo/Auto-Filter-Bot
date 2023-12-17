5aea4a6ae944847060210b4bd678ff53cc66958a
Aninkoo <112639445+Aninkoo@users.noreply.github.com>
2023-12-17 21:26:43 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -104,7 +104,7 @@ async def start(client, message):
     
     verify_status = await get_verify_status(message.from_user.id)
     if IS_VERIFY and not verify_status['is_verified']:
-        token = cpagrip
+        token = 'cpagrip'
         await update_verify_status(message.from_user.id, verify_token=token, link="" if mc == 'inline_verify' else mc)
         link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API, f'https://t.me/{temp.U_NAME}?start=verify_{token}')
         btn = [[
