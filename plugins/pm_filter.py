41f963db29b9da43928309c7610266f23a590a54
Aninkoo <112639445+Aninkoo@users.noreply.github.com>
2024-01-06 00:26:56 +0100

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -332,7 +332,7 @@ async def advantage_spoll_choker(bot, query):
         ]]
         reply_markup = InlineKeyboardMarkup(btn)
         k = await query.message.edit(
-            f"database:😔 File not Found\n\nDo you want to Request for this Movie?",
+            f"database:😔File not Found!\n\nDo you want to Request for this Movie?",
             reply_markup = reply_markup)
         if query.data == "request":
             await bot.send_message(LOG_CHANNEL, script.NO_RESULT_TXT.format(query.message.chat.title, query.message.chat.id, query.from_user.mention, search))
